2b7c38d1b4dfd4823ad39bc7ad493778ab8a6587
Koimi YOVO <yovoedem33@gmail.com>
2020-08-14 10:11:47 +0200

    Add grouping on number of students by enrollement date on "About" page

---
+++ Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime? EnrollmentDate { get; set; }
+
+        [Display(Name = "Students")]
+        public int StudentCount { get; set; }
+    }
+}
