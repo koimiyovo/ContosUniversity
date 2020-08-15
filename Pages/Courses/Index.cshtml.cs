24a5668a1dc83337c49cd6aa7386e6cf1601a988
Koimi YOVO <yovoedem33@gmail.com>
2020-08-15 20:45:40 +0200

    Create razor pages for Courses and Instructors

---
+++ Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
+
+namespace ContosoUniversity
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        //public IList<Course> Courses { get;set; }
+
+        public IList<CourseViewModel> CourseVM { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            /*Courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();*/
+
+            CourseVM = await _context.Courses
+                               .Select(c => new CourseViewModel
+                               {
+                                   CourseID = c.CourseID,
+                                   Credits = c.Credits,
+                                   DepartmentName = c.Department.Name,
+                                   Title = c.Title
+                               })
+                               .ToListAsync();
+        }
+    }
+}
