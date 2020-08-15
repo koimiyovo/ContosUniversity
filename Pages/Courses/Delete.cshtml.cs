88bd035b1ba6b74e9781ad735682ad60da994f5a
Koimi YOVO <yovoedem33@gmail.com>
2020-08-15 21:35:55 +0200

    Update Courses/Delete page

--- Pages/Courses/Delete.cshtml.cs
+++ Pages/Courses/Delete.cshtml.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
 namespace ContosoUniversity
@@ -30,7 +26,9 @@ namespace ContosoUniversity
             }
 
             Course = await _context.Courses
-                .Include(c => c.Department).FirstOrDefaultAsync(m => m.CourseID == id);
+                .AsNoTracking()
+                .Include(c => c.Department)
+                .FirstOrDefaultAsync(m => m.CourseID == id);
 
             if (Course == null)
             {
