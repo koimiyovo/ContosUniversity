bcdea6f0544091c654d99cb19123ae1a6eac67d0
Koimi YOVO <yovoedem33@gmail.com>
2020-08-15 21:34:21 +0200

    Create a page model to list the department names in a select list

---
+++ Pages/Courses/DepartmentNamePageModel.cs
@@ -0,0 +1,21 @@
+﻿using ContosoUniversity.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class DepartmentNamePageModel : PageModel
+    {
+        public SelectList DepartmentNameSL { get; set; }
+
+        public void PopulateDepartmentDropDownList(SchoolContext _context, object selectedDepartment = null)
+        {
+            var departmentQuery = from d in _context.Departments
+                                  orderby d.Name
+                                  select d;
+
+            DepartmentNameSL = new SelectList(departmentQuery, "DepartmentID", "Name", selectedDepartment);
+        }
+    }
+}
